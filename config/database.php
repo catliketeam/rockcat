a95f8e29b7ac888a7e7a89899a339b23b6e6e1c7
Richard Kuiper <apyrael@gmail.com>
2025-04-19 23:21:26 +0200

    Forced ssl

--- config/database.php
+++ config/database.php
@@ -141,6 +141,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port'     => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DATABASE', 0),
+            'ssl'      => ['verify_peer' => false],
         ],
 
         'session' => [
